769e803365db184b5e5e728b7ebe79d9c1c4fe82
drgtdrgtgsd <nxhs3995@gmail.com>
2026-09-09 19:56:36 +0800

    Convert Twitter phone modal to use native HTML layout driven by Cloudflare worker proxying Nitter RSS

---
+++ source/_posts/https审查环境下最后的一块拼图ech.md
@@ -0,0 +1,10 @@
+---
+title: https审查环境下最后的一块拼图-ech
+cover: "https://user-images.githubusercontent.com/58414694/154399776-6790111e-3646-4e25-9ddb-2e1ad2975feb.png"
+tags:
+- 网络
+- https
+- 隐私
+---
+# 首言
+之前讲过，tls握手中，虽内容加密，但是sni处于明文
---
+++ worker.js
@@ -0,0 +1,35 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/twitter/")) {
+      const username = url.pathname.split("/").pop();
+      const instances = [
+        \https://nitter.poast.org/\/rss\,
+        \https://nitter.privacydev.net/\/rss\,
+        \https://xcancel.com/\/rss\
+      ];
+      
+      let xmlText = "";
+      for (const instance of instances) {
+        try {
+          const response = await fetch(instance, {
+            headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' }
+          });
+          if (response.ok) {
+            xmlText = await response.text();
+            break;
+          }
+        } catch (e) {
+          continue;
+        }
+      }
+      
+      return new Response(xmlText || '<error>Failed to fetch RSS</error>', {
+        headers: { 'Content-Type': 'application/xml', 'Access-Control-Allow-Origin': '*' }
+      });
+    }
+
+    return env.ASSETS.fetch(request);
+  }
+};
